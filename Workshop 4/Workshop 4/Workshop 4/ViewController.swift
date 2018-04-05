b18fbf190edb7e794554251646eb75d29b8f6154
issd <s.vansomeren@student.fontys.nl>
2018-04-05 10:49:29 +0200

    vergeten de finished versie van workshop 4 te pushen..

--- Workshop 4/Workshop 4/Workshop 4/ViewController.swift
+++ Workshop 4/Workshop 4/Workshop 4/ViewController.swift
@@ -5,21 +5,41 @@
 //  Created by issd on 16/03/2018.
 //  Copyright © 2018 issd. All rights reserved.
 //
-
+import CoreLocation
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    
+    @IBOutlet var mapView: MKMapView!
+    let annotation = MKPointAnnotation()
+    let annotationCircle = MKCircle();
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation = locations[0]
+        mapView.setCenter(newLocation.coordinate, animated: true)
+        annotation.coordinate = CLLocationCoordinate2D(latitude: newLocation.coordinate.latitude, longitude: newLocation.coordinate.longitude)
+        mapView.addAnnotation(annotation)
+    
     }
+    
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
